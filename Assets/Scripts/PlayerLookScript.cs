3c1448b5233e7957dd42eae6ca03590006dc3ea2
RAINEEN20 <raineen20@gcc.edu>
2023-03-21 19:44:28 -0400

    Add darkness and fog

--- Assets/Scripts/PlayerLookScript.cs
+++ Assets/Scripts/PlayerLookScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerLookScript : MonoBehaviour
 {
+    Transform _transform;
+    float verticalAngle = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalAngle = _transform.eulerAngles.y + (Input.GetAxis("Mouse X") * 4);
+            verticalAngle -= (Input.GetAxis("Mouse Y") * 4);
+            verticalAngle = Mathf.Clamp(verticalAngle, -60, 60);
+
+            _transform.rotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0);
     }
 }
