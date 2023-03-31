84989d43a829b07585fcd8d8e2c2b690b3ce6221
RAINEEN20 <raineen20@gcc.edu>
2023-03-31 11:47:41 -0400

    Fix lag and small bugs

--- Assets/Scripts/PlayerLookScript.cs
+++ Assets/Scripts/PlayerLookScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLookScript : MonoBehaviour
 {
@@ -9,22 +10,28 @@ public class PlayerLookScript : MonoBehaviour
     public Transform _headTransform;
     float verticalAngle = 0;
     public LayerMask _layer;
+    PlayerMoveScript _player;
+    public Text _pickupText;
     // Start is called before the first frame update
     void Start()
     {
+        _player = FindObjectOfType<PlayerMoveScript>();
         _transform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalAngle = _transform.eulerAngles.y + (Input.GetAxis("Mouse X") * 4);
+        if(_player._movement)
+        {
+            float horizontalAngle = _transform.eulerAngles.y + (Input.GetAxis("Mouse X") * 4);
             verticalAngle -= (Input.GetAxis("Mouse Y") * 4);
             verticalAngle = Mathf.Clamp(verticalAngle, -60, 60);
 
             //_transform.rotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0);
             _transform.rotation = Quaternion.Euler(0, horizontalAngle, 0);
             _headTransform.localRotation = Quaternion.Euler(verticalAngle, 0, 0);
+        }
 
         //Look Ray
         var ray = new Ray(_headTransform.position, _headTransform.forward);
@@ -32,6 +39,11 @@ public class PlayerLookScript : MonoBehaviour
         if (Physics.Raycast(ray, out hit, 15)) 
         {
             _hit = hit.transform.gameObject;
+        }
+
+        if(_hit.gameObject.tag != "Note" && _hit.gameObject.tag != "StoryNote")
+        {
+            _pickupText.enabled = false;
         }
 
     }
